fc938990fc00bc151e24a4695a97b6691b65cdf5
PMA <pierre-marie.allard@unifr.ch>
2021-09-06 21:35:35 +0200

    adding pivot

--- src/dev/met_annot_enhancer_plotter.py
+++ src/dev/met_annot_enhancer_plotter.py
@@ -17,7 +17,7 @@ from pandas import json_normalize
 import yaml
 import spectral_lib_matcher
 
-# for debug ony shuld be commented later
+# for debug ony shuld be commented later 
 from pathlib import Path
 p = Path(__file__).parents[2]
 print(p)
@@ -71,6 +71,8 @@ file_extension = params_list['repond_params'][8]['file_extension']
 msfile_suffix = params_list['repond_params'][9]['msfile_suffix']
 min_score_taxo_ms1 = params_list['repond_params'][10]['min_score_taxo_ms1']
 min_score_chemo_ms1 = params_list['repond_params'][11]['min_score_chemo_ms1']
+
+drop_blanks = params_list['plotting_params'][0]['drop_blanks']
 
 
 
@@ -132,17 +134,36 @@ isdb_results_repond_path = os.path.join(path_to_results_folders, spectral_match_
 spectral_match_results_repond_flat_filename = project_name + '_spectral_match_results_repond_flat.tsv'
 isdb_results_repond_flat_path = os.path.join(path_to_results_folders, spectral_match_results_repond_flat_filename)
 
+
+spectral_match_results_repond_flat_sel_filename = project_name + '_spectral_match_results_repond_flat_selected.tsv'
+isdb_results_repond_flat_sel_path = os.path.join(path_to_results_folders, spectral_match_results_repond_flat_sel_filename)
+
+
+
+
+
 sunburst_chem_filename = project_name + '_chemo_sunburst.html'
 sunburst_organisms_filename = project_name + '_organisms_sunburst.html'
+
 treemap_chemo_counted_filename = project_name + '_chemo_treemap_counted.html'
 treemap_chemo_intensity_filename = project_name + '_chemo_treemap_intensity.html'
 
+treemap_chemo_multi_counted_filename = project_name + '_chemo_treemap_multi_counted.html'
+treemap_chemo_multi_intensity_filename = project_name + '_chemo_treemap_multi_intensity.html'
+
+pivot_table_filename = project_name + '_pivot_table.html'
+
+
 sunburst_chem_results_path = os.path.join(path_to_results_folders,sunburst_chem_filename)
 sunburst_organisms_results_path = os.path.join(path_to_results_folders,sunburst_organisms_filename)
+
 treemap_chemo_counted_results_path = os.path.join(path_to_results_folders,treemap_chemo_counted_filename)
 treemap_chemo_intensity_results_path = os.path.join(path_to_results_folders,treemap_chemo_intensity_filename)
 
+treemap_chemo_multi_counted_results_path = os.path.join(path_to_results_folders,treemap_chemo_multi_counted_filename)
+treemap_chemo_multi_intensity_results_path = os.path.join(path_to_results_folders,treemap_chemo_multi_intensity_filename)
 
+pivot_table_results_path = os.path.join(path_to_results_folders,pivot_table_filename)
 
 
 # Writing used parameters 
@@ -733,6 +754,8 @@ if output_plots == True:
     ''')
 
     import plotly.express as px
+    from plotly.subplots import make_subplots
+    import plotly.graph_objects as go
 
     fig = px.sunburst(df4cyto_flat, path=['structure_taxonomy_npclassifier_01pathway_consensus', 'structure_taxonomy_npclassifier_02superclass_consensus', 'structure_taxonomy_npclassifier_03class_consensus'])
     fig.update_layout(
@@ -798,113 +821,209 @@ if output_plots == True:
                     include_plotlyjs='cdn')
 
 
-# here we want to have an puput per sample so we merge back the annotation frame with the feature table 
-#actually we might want to have the metadat joined to the feature table since the beginning
+    # here we want to have an puput per sample so we merge back the annotation frame with the feature table 
+    #actually we might want to have the metadat joined to the feature table since the beginning
 
-feature_intensity_table_t
+    feature_intensity_table_t
 
-samples_metadata_full = pd.read_csv(metadata_table_path + str(os.listdir(metadata_table_path)[0]), sep='\t')
-feature_intensity_meta = pd.merge(left=samples_metadata_full, right=feature_intensity_table_t, left_on='filename', right_on='MS_filename',how='inner')
-
-
-feature_intensity_meta_gp_species = feature_intensity_meta.groupby(organism_header).mean()
-feature_intensity_meta_gp_species = feature_intensity_meta_gp_species.transpose()
-feature_intensity_meta_gp_species.index.name = 'row_ID'
-
-feature_intensity_table = feature_intensity_table_t.transpose()
-
-feature_intensity_table.reset_index(inplace=True)
-feature_intensity_meta_gp_species.reset_index(inplace=True)
+    samples_metadata_full = pd.read_csv(metadata_table_path + str(os.listdir(metadata_table_path)[0]), sep='\t')
+    feature_intensity_meta = pd.merge(left=samples_metadata_full, right=feature_intensity_table_t, left_on='filename', right_on='MS_filename',how='inner')
 
 
-ft_merged = pd.merge(feature_intensity_table, feature_intensity_meta_gp_species, on='row_ID', how='left')
+    feature_intensity_meta_gp_species = feature_intensity_meta.groupby(organism_header).mean()
+    feature_intensity_meta_gp_species = feature_intensity_meta_gp_species.transpose()
+    feature_intensity_meta_gp_species.index.name = 'row_ID'
 
+    feature_intensity_meta_gp_multi = feature_intensity_meta.groupby([organism_header,sampletype_header]).mean()
+    feature_intensity_meta_gp_multi = feature_intensity_meta_gp_multi.transpose()
+    feature_intensity_meta_gp_multi.columns = feature_intensity_meta_gp_multi.columns.map('_'.join)
+    feature_intensity_meta_gp_multi.index.name = 'row_ID'
 
-ft_merged = pd.merge(feature_intensity_table, feature_intensity_meta_gp_species, on='row_ID', how='left')
+    feature_intensity_table = feature_intensity_table_t.transpose()
 
-df4cyto_flat['feature_id'] = df4cyto_flat['feature_id'].astype('int')
+    feature_intensity_table.reset_index(inplace=True)
+    feature_intensity_meta_gp_species.reset_index(inplace=True)
+    feature_intensity_meta_gp_multi.reset_index(inplace=True)
+    
 
-dt_isdb_results_int = pd.merge(
-    df4cyto_flat, ft_merged, left_on='feature_id', right_on='row_ID', how='left')
+    ft_merged = pd.merge(feature_intensity_table, feature_intensity_meta_gp_species, on='row_ID', how='left')
+    ft_merged = pd.merge(ft_merged, feature_intensity_meta_gp_multi, on='row_ID', how='left')
 
-dt_isdb_results_int['counter'] = 1
+    df4cyto_flat['feature_id'] = df4cyto_flat['feature_id'].astype('int')
 
+    dt_isdb_results_int = pd.merge(
+        df4cyto_flat, ft_merged, left_on='feature_id', right_on='row_ID', how='left')
 
-
-
-# for n in samples_metadata_full['species_name'].unique():
-#     print(n)
-
-#     dt = dt_isdb_results_int[dt_isdb_results_int[n] > 0]
-
-#     fig = px.treemap(dt, path=[px.Constant("all"), 'structure_taxonomy_npclassifier_01pathway', 'structure_taxonomy_npclassifier_02superclass', 'structure_taxonomy_npclassifier_03class'], values=n)
-#     fig.update_traces(root_color="lightgrey")
-#     fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
-#     fig.show()
-
-
-# for n in samples_metadata_full['species_name'].unique():
-#     print(n)
-
-#     dt = dt_isdb_results_int[dt_isdb_results_int[n] > 0]
-
-#     fig = px.treemap(dt, path=[px.Constant("all"), 'structure_taxonomy_npclassifier_01pathway', 'structure_taxonomy_npclassifier_02superclass', 'structure_taxonomy_npclassifier_03class'], values='counter')
-#     fig.update_traces(root_color="lightgrey")
-#     fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
-#     fig.show()
+    dt_isdb_results_int['counter'] = 1
 
 
 
-# here we also need to incresae the spec list of list of dic according to the lenght of unique_group_labels this can be done following https://stackoverflow.com/a/3459131 
-# in fact we do this with itertools
 
-import itertools
-unique_group_labels = samples_metadata_full[organism_header].unique()
+    # for n in samples_metadata_full['species_name'].unique():
+    #     print(n)
 
-pattern=[{"type": "domain"}]
+    #     dt = dt_isdb_results_int[dt_isdb_results_int[n] > 0]
 
-
-rep_pattern = list(itertools.chain.from_iterable(itertools.repeat(x, len(unique_group_labels)) for x in pattern))
-
-
-fig = make_subplots(1, len(unique_group_labels),
-subplot_titles = (unique_group_labels),
-specs=[rep_pattern])
-
-i=1
-for n in unique_group_labels:
-    print(n)
-
-    dt = dt_isdb_results_int[dt_isdb_results_int[n] > 0]
-    fig.add_trace(px.treemap(dt, path=[px.Constant("all"), 'structure_taxonomy_npclassifier_01pathway', 'structure_taxonomy_npclassifier_02superclass', 'structure_taxonomy_npclassifier_03class'], 
-    values='counter').data[0], 
-    row=1,col=i)
-    i+=1
-
-fig.update_traces(root_color="lightgrey")
-fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
-fig.update_annotations(font_size=12)
-fig.show()
-fig.write_html(treemap_chemo_counted_results_path,
-            full_html=False,
-            include_plotlyjs='cdn')
+    #     fig = px.treemap(dt, path=[px.Constant("all"), 'structure_taxonomy_npclassifier_01pathway', 'structure_taxonomy_npclassifier_02superclass', 'structure_taxonomy_npclassifier_03class'], values=n)
+    #     fig.update_traces(root_color="lightgrey")
+    #     fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+    #     fig.show()
 
 
-i=1
-for n in unique_group_labels:
-    print(n)
+    # for n in samples_metadata_full['species_name'].unique():
+    #     print(n)
 
-    dt = dt_isdb_results_int[dt_isdb_results_int[n] > 0]
-    fig.add_trace(px.treemap(dt, path=[px.Constant("all"), 'structure_taxonomy_npclassifier_01pathway', 'structure_taxonomy_npclassifier_02superclass', 'structure_taxonomy_npclassifier_03class'], 
-    values=n).data[0], 
-    row=1,col=i)
-    i+=1
+    #     dt = dt_isdb_results_int[dt_isdb_results_int[n] > 0]
 
-fig.update_traces(root_color="lightgrey")
-fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
-fig.update_annotations(font_size=12)
-fig.show()
-fig.write_html(treemap_chemo_intensity_results_path,
-            full_html=False,
-            include_plotlyjs='cdn')
+    #     fig = px.treemap(dt, path=[px.Constant("all"), 'structure_taxonomy_npclassifier_01pathway', 'structure_taxonomy_npclassifier_02superclass', 'structure_taxonomy_npclassifier_03class'], values='counter')
+    #     fig.update_traces(root_color="lightgrey")
+    #     fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+    #     fig.show()
 
+
+
+    # here we also need to incresae the spec list of list of dic according to the lenght of unique_group_labels this can be done following https://stackoverflow.com/a/3459131 
+    # in fact we do this with itertools
+    drop_blanks = True
+    if drop_blanks == True:
+        samples_metadata_full = samples_metadata_full[~samples_metadata_full[organism_header].str.contains("none|BK|blanck")]
+
+    import itertools
+    unique_group_labels = samples_metadata_full[organism_header].unique()
+
+    pattern=[{"type": "domain"}]
+
+
+    rep_pattern = list(itertools.chain.from_iterable(itertools.repeat(x, len(unique_group_labels)) for x in pattern))
+
+
+    fig = make_subplots(1, len(unique_group_labels),
+    subplot_titles = (unique_group_labels),
+    specs=[rep_pattern])
+
+    i=1
+    for n in unique_group_labels:
+        print(n)
+
+        dt = dt_isdb_results_int[dt_isdb_results_int[n] > 0]
+        fig.add_trace(px.treemap(dt, path=[px.Constant("all"), 'structure_taxonomy_npclassifier_01pathway', 'structure_taxonomy_npclassifier_02superclass', 'structure_taxonomy_npclassifier_03class'], 
+        values='counter').data[0], 
+        row=1,col=i)
+        i+=1
+
+    fig.update_traces(root_color="lightgrey")
+    fig.update_layout(margin = dict(t=50, l=25, r=25, b=25),
+    title_text="Metabolite annotation overview (size proportional to individual count)")
+    fig.update_annotations(font_size=12)
+    fig.show()
+    fig.write_html(treemap_chemo_counted_results_path,
+                full_html=False,
+                include_plotlyjs='cdn')
+
+    fig = make_subplots(1, len(unique_group_labels),
+    subplot_titles = (unique_group_labels),
+    specs=[rep_pattern])
+
+    i=1
+    for n in unique_group_labels:
+        print(n)
+
+        dt = dt_isdb_results_int[dt_isdb_results_int[n] > 0]
+        fig.add_trace(px.treemap(dt, path=[px.Constant("all"), 'structure_taxonomy_npclassifier_01pathway', 'structure_taxonomy_npclassifier_02superclass', 'structure_taxonomy_npclassifier_03class'], 
+        values=n).data[0], 
+        row=1,col=i)
+        i+=1
+
+    fig.update_traces(root_color="lightgrey")
+    fig.update_layout(margin = dict(t=50, l=25, r=25, b=25),
+    title_text="Metabolite annotation overview (size proportional to mean intensity)")
+    fig.update_annotations(font_size=12)
+    fig.show()
+    fig.write_html(treemap_chemo_intensity_results_path,
+                full_html=False,
+                include_plotlyjs='cdn')
+
+#### working on the multilabelled subset
+    samples_metadata_full['combined'] = samples_metadata_full[organism_header] + '_' + samples_metadata_full[sampletype_header]
+    unique_group_labels = samples_metadata_full['combined'].unique()
+    type(unique_group_labels)
+
+    pattern=[{"type": "domain"}]
+
+
+    rep_pattern = list(itertools.chain.from_iterable(itertools.repeat(x, len(unique_group_labels)) for x in pattern))
+
+    fig = make_subplots(1, len(unique_group_labels),
+    subplot_titles = (unique_group_labels),
+    specs=[rep_pattern])
+
+    i=1
+    for n in unique_group_labels:
+        print(n)
+
+        dt = dt_isdb_results_int[dt_isdb_results_int[n] > 0]
+        fig.add_trace(px.treemap(dt, path=[px.Constant("all"), 'structure_taxonomy_npclassifier_01pathway', 'structure_taxonomy_npclassifier_02superclass', 'structure_taxonomy_npclassifier_03class'], 
+        values=n).data[0], 
+        row=1,col=i)
+        i+=1
+
+    fig.update_traces(root_color="lightgrey")
+    fig.update_layout(margin = dict(t=50, l=25, r=25, b=25),
+    title_text="Metabolite annotation overview (size proportional to individual count)")
+    fig.update_annotations(font_size=12)
+    fig.show()
+    fig.write_html(treemap_chemo_multi_counted_results_path,
+                full_html=False,
+                include_plotlyjs='cdn')
+
+
+
+    fig = make_subplots(1, len(unique_group_labels),
+    subplot_titles = (unique_group_labels),
+    specs=[rep_pattern])
+
+    i=1
+    for n in unique_group_labels:
+        print(n)
+
+        dt = dt_isdb_results_int[dt_isdb_results_int[n] > 0]
+        fig.add_trace(px.treemap(dt, path=[px.Constant("all"), 'structure_taxonomy_npclassifier_01pathway', 'structure_taxonomy_npclassifier_02superclass', 'structure_taxonomy_npclassifier_03class'], 
+        values=n).data[0], 
+        row=1,col=i)
+        i+=1
+
+    fig.update_traces(root_color="lightgrey")
+    fig.update_layout(margin = dict(t=50, l=25, r=25, b=25),
+    title_text="Metabolite annotation overview (size proportional to mean intensity)")
+    fig.update_annotations(font_size=12)
+    fig.show()
+    fig.write_html(treemap_chemo_multi_intensity_results_path,
+                full_html=False,
+                include_plotlyjs='cdn')
+
+
+df4cyto_flat['final_score'] = df4cyto_flat['final_score'].astype('float')
+df4cyto_flat[df4cyto_flat['final_score'] >= 8]
+
+df4cyto_flat.columns
+
+
+df4cyto_flat_sel = df4cyto_flat[['feature_id', 'component_id', 'structure_taxonomy_npclassifier_01pathway_consensus','structure_taxonomy_npclassifier_02superclass_consensus',
+'structure_taxonomy_npclassifier_03class_consensus', 'score_input', 'libname',
+'structure_inchikey', 'structure_inchi', 'structure_smiles', 'structure_molecular_formula',
+'adduct', 'structure_exact_mass', 'short_inchikey',
+'structure_taxonomy_npclassifier_01pathway', 'structure_taxonomy_npclassifier_02superclass',
+'structure_taxonomy_npclassifier_03class', 'organism_name', 'organism_taxonomy_ottid',
+'organism_taxonomy_01domain', 'organism_taxonomy_02kingdom', 'organism_taxonomy_03phylum',
+'organism_taxonomy_04class', 'organism_taxonomy_05order', 'organism_taxonomy_06family',
+'organism_taxonomy_07tribe', 'organism_taxonomy_08genus', 'organism_taxonomy_09species',
+'score_taxo', 'score_max_consistency', 'final_score']]
+
+
+
+df4cyto_flat_sel.to_csv(isdb_results_repond_flat_sel_path, sep='\t', index=None)
+
+
+from pivottablejs import pivot_ui
+
+pivot_ui(df4cyto_flat_sel, outfile_path=pivot_table_results_path)
